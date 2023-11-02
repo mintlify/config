ab942138d89a0f0ef31212fdb04e914c0ed2634e
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2023-11-01 17:18:21 -0700

    feat: add type rules (#21)
    added some rules to prevent common pitfalls:
    
    - prevent conditionals that are always `false` / always `true`, like
    this one that I did:
    <img width="1048" alt="Screenshot 2023-11-01 at 5 09 33 PM"
    src="https://github.com/mintlify/config/assets/63772591/bba29831-0d9d-406d-a346-f952bfdc9e57">
    
    - prevent unawaited promises, which can cause weird race condition bugs
    - disallow implicit any

--- packages/eslint-config-typescript/config.js
+++ packages/eslint-config-typescript/config.js
@@ -4,5 +4,8 @@ module.exports = {
   extends: ["@mintlify/eslint-config", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-empty-function": "off", // this rule causes eslint to crash, see: https://stackoverflow.com/a/76511985/20170974
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-unnecessary-condition": "error",
   },
 };
