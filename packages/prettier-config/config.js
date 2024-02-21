d4e148e2e7617839b329c81cae96bc215b4d4ac2
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2024-02-20 21:32:19 -0500

    add prettier-plugin-tailwindcss (#27)
    adding prettier-plugin-tailwindcss to auto sort tailwind classes
    
    https://tailwindcss.com/blog/automatic-class-sorting-with-prettier

--- packages/prettier-config/config.js
+++ packages/prettier-config/config.js
@@ -9,7 +9,7 @@ module.exports = {
   jsxSingleQuote: false,
   bracketSpacing: true,
   bracketSameLine: false,
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
   importOrder: ["<THIRD_PARTY_MODULES>", "^@/(.*)$", "^[./]"],
-  importOrderSeparation: true,
+  importOrderSeparation: true
 };
