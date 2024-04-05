68138872cb18d51f16d56e9b606889b59f321e85
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-04-04 23:01:40 -0700

    feat: add no for in array (#28)
    Just almost got burned by this for the nth time, can't believe this rule
    isn't in the ts eslint recommended

--- packages/eslint-config-typescript/config.js
+++ packages/eslint-config-typescript/config.js
@@ -7,5 +7,6 @@ module.exports = {
     "@typescript-eslint/no-empty-function": "off", // this rule causes eslint to crash, see: https://stackoverflow.com/a/76511985/20170974
     "@typescript-eslint/no-floating-promises": "error",
     "@typescript-eslint/no-unnecessary-condition": "error",
+    "@typescript-eslint/no-for-in-array": "error",
   },
 };
