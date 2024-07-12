1f9d0ce3d967c6e8a8588bf97a11b8f39bc0e527
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2024-07-12 16:24:43 -0700

    Add eslint-config-typescript-react (#30)
    Add configs for ts + react apps (that is not nextjs based)
    
    ---------
    
    Co-authored-by: Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>

---
+++ packages/eslint-config-typescript-react/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:jsx-a11y/recommended", "@mintlify/eslint-config-typescript"],
+  rules: {
+    "react/no-unknown-property": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "jsx-a11y/alt-text": [
+      "warn",
+      {
+        elements: ["img"],
+        img: ["Image"],
+      },
+    ],
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+    "react/jsx-no-target-blank": "off",
+  },
+}
--- packages/eslint-config-typescript/config.js
+++ packages/eslint-config-typescript/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "import"],
   extends: ["@mintlify/eslint-config", "plugin:@typescript-eslint/recommended"],
   rules: {
+    "import/no-anonymous-default-export": "warn",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-empty-function": "off", // this rule causes eslint to crash, see: https://stackoverflow.com/a/76511985/20170974
     "@typescript-eslint/no-floating-promises": "error",
