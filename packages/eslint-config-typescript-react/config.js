5f51324eeb3b939024f610da934141ed4777e2a5
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2024-07-12 15:39:28 -0700

    add rules
    https://github.com/vercel/next.js/blob/canary/packages/eslint-config-next/index.js

--- packages/eslint-config-typescript-react/config.js
+++ packages/eslint-config-typescript-react/config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  extends: ["@mintlify/eslint-config-typescript", "plugin:react/recommended", "plugin:react-hooks/recommended"],
-};
+  extends: ["@mintlify/eslint-config-typescript", "jsx-a11y", "plugin:import/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"],
+  rules: {
+    'import/no-anonymous-default-export': 'warn',
+    'react/no-unknown-property': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'react/jsx-no-target-blank': 'off',
+  },
+}
