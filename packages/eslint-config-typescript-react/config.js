1bac359f8b9a1d89b716ba2360d7fc725c5323c9
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2024-07-12 15:56:05 -0700

    move import/recommended to ts

--- packages/eslint-config-typescript-react/config.js
+++ packages/eslint-config-typescript-react/config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  extends: ["@mintlify/eslint-config-typescript", "jsx-a11y", "plugin:import/recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"],
+  plugins: ["react/recommended", "react-hooks/recommended"],
+  extends: ["@mintlify/eslint-config-typescript", "jsx-a11y"],
   rules: {
-    'import/no-anonymous-default-export': 'warn',
-    'react/no-unknown-property': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/alt-text': [
-      'warn',
+    "react/no-unknown-property": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "jsx-a11y/alt-text": [
+      "warn",
       {
-        elements: ['img'],
-        img: ['Image'],
+        elements: ["img"],
+        img: ["Image"],
       },
     ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
-    'react/jsx-no-target-blank': 'off',
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+    "react/jsx-no-target-blank": "off",
   },
 }
