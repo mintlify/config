0670cc3e68dddfffbb4c0b432f4e4f659d11df85
Leyland Yang <105257885+yangleyland@users.noreply.github.com>
2026-01-16 11:19:30 -0800

    add no misused promises (#36)
    - add no misused promises eslint rule to prevent promises from being
    used as a boolean

--- packages/eslint-config-typescript/config.js
+++ packages/eslint-config-typescript/config.js
@@ -7,6 +7,13 @@ module.exports = {
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-empty-function": "off", // this rule causes eslint to crash, see: https://stackoverflow.com/a/76511985/20170974
     "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksConditionals: true,
+        checksVoidReturn: false,
+      },
+    ],
     "@typescript-eslint/no-unnecessary-condition": "error",
     "@typescript-eslint/no-for-in-array": "error",
   },
